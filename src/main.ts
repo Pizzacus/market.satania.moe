094dcc3cc64b0189f97076ebea466bc8749a0c4f
Pizzacus <mail@pizzacus.com>
2020-12-20 17:34:22 +0100

    Add CSS vars

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import "./index.css";
 import "bootstrap/dist/css/bootstrap-reboot.min.css";
+import "./index.css";
 
 createApp(App).use(router).mount('#app')
