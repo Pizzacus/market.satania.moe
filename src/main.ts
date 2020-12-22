10013c51773fee5490a5cf8e6a60fcf2c74a4e17
Pizzacus <mail@pizzacus.com>
2020-12-22 15:02:27 +0100

    Add fonts, change page title, move CSS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 
 import "bootstrap/dist/css/bootstrap-reboot.min.css";
-import "./index.css";
+import "source-sans-pro/source-sans-pro.css";
+import "source-sans-pro/source-sans-variable.css";
+import "fontsource-asap/600.css";
+import "./styles/index.css";
 
 createApp(App).use(router).mount('#app')
