a0ddd1250c09e4d238187650742826c7e55c4a82
Pizzacus <mail@pizzacus.com>
2020-12-26 21:34:15 +0100

    Country code should be lowercase

--- src/get-country.ts
+++ src/get-country.ts
@@ -2,6 +2,6 @@ export default function getCountry(): Promise<null | string> {
     return fetch("/", { method: "HEAD" })
         .then(res => {
 			// This header is returned by BunnyCDN and contains the IP country!
-            return res.headers.get("cdn-requestcountrycode");
+            return res.headers.get("cdn-requestcountrycode")?.toLowerCase() ?? null;
         })
 }
