8018de4a1aaaac9ab178c6d32c9b10a6dd33d54f
Pizzacus <mail@pizzacus.com>
2020-12-26 18:05:09 +0100

    Add product JSON

---
+++ src/products.types.ts
@@ -0,0 +1,22 @@
+export interface Product {
+	// based on SnipCart's ProductDefinition
+	// https://docs.snipcart.com/v3/sdk/reference#core-models-ProductDefinition
+	id: string;
+	name: string;
+	price: Record<string, number>;
+	url: string;
+	description: string;
+	image: string;
+	metadata: {
+		// We can store any data we want here
+		detailedImages: string[];
+		featuredImageIndex: number;
+		productType: "hoodie" | "shirt";
+		closeupImage: string;
+	};
+	customFields: {
+		name: string;
+		options: string;
+		type: string;
+	}[];
+}
