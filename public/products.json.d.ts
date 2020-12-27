87b3e6200b2933f3d38e9f5199bfe951717668f8
Pizzacus <mail@pizzacus.com>
2020-12-27 16:29:59 +0100

    Ah ok thats how you make delcration files

--- public/products.json.d.ts
+++ public/products.json.d.ts
@@ -1,6 +1,4 @@
 import type { Product } from "../src/products.types";
 
-declare module "products.json" {
-	const products: Product[];
-	export default products;
-}
+declare const products: Product[];
+export default products;
