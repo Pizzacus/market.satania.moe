c42039b68c6d97e7f7d219d4408bdcd85b747747
Pizzacus <mail@pizzacus.com>
2021-01-01 02:14:50 +0100

    Update packages

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ import router from "./router";
 import "bootstrap/dist/css/bootstrap-reboot.min.css";
 import "source-sans-pro/source-sans-pro.css";
 import "source-sans-pro/source-sans-variable.css";
-import "fontsource-asap/600.css";
+import "@fontsource/asap/600.css";
 import "./styles/index.css";
 
 createApp(App).use(router).mount('#app')
